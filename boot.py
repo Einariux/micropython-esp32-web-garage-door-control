76f17250d43325c9de2a32280930d0bf8efdbc71
Einariux <einas39@gmail.com>
2023-11-14 10:27:29 +0200

    Dadejau I2C sasaja davikliams,ekranui

--- boot.py
+++ boot.py
@@ -1,5 +1,5 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-from machine import Pin
+from machine import Pin, I2C, RTC
 from vartu_wifi_nustatymai import VartuWIFI
 import utime
 import esp
@@ -8,3 +8,4 @@ vw = VartuWIFI()
 vw.connect()
 import vartu_valdymas
 led = Pin(2, Pin.OUT)
+i2c = I2C(1, scl=Pin(22), sda=Pin(21), freq=400000)
